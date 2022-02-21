ad4190c7a9fe5ac2251b680475c79bda6bf892f5
yehjim <yeh199990402@gmail.com>
2022-02-21 22:18:46 +0800

    scrolling

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-
 var app = new Vue({
-    el:'#app',
-    data:{
-        fullpageshow:false
+    el: '#app',
+    data: {
+        fullpageshow: false
     },
     methods: {
-        closefullpagemenu(){
-            console.log('123')
-            this.fullpageshow = false
-        },
-        openfullpagemenu(){
-            this.fullpageshow = true
+        openfullpagemenu() {
+            if (this.fullpageshow == true) {
+                this.fullpageshow = false
+                anime({
+                    targets: '.hammenu .line:nth-child(1) ',
+                    backgroundColor: '#e07d6a',
+                    rotate: 0,
+                    translateY:0,
+                    translateX:0,
+                    duration: 1000
+                });
+                anime({
+                    targets: '.hammenu .line:nth-child(2) ',
+                    backgroundColor: '#e07d6a',
+                    rotate: 0,
+                    translateY:0,
+                    translateX:0,
+                    duration: 1000
+                });
+            } else {
+                this.fullpageshow = true
+                anime({
+                    targets: '.fullpagemenu',
+                    opacity: [0, 1],
+                    duration: 2000
+                });
+                anime({
+                    targets: '.hammenu .line:nth-child(1) ',
+                    backgroundColor: '#fff',
+                    rotate: 30,
+                    translateY:2,
+                    translateX:13,
+                    duration: 1000
+                });
+                anime({
+                    targets: '.hammenu .line:nth-child(2) ',
+                    backgroundColor: '#fff',
+                    rotate: -30,
+                    translateX:13,
+                    translateY:-10,
+                    duration: 1000
+                });
+            }
+
         }
     },
 })
@@ -19,24 +56,25 @@ gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline();
 tl.to(".pic > img", {
-    // x:1000,
-    opacity: 0,
-    
-    duration: 1.5,
-    scale:0.9
-})
-.to(".logo",{
-    opacity:1,
-    duration:1.5,
-    // scale:1
-})
+        // x:1000,
+        opacity: 0,
+
+        duration: 1.5,
+        scale: 0.9
+    })
+    .to(".logo", {
+        opacity: 1,
+        duration: 1.5,
+        // scale:1
+    })
+
 
 ScrollTrigger.create({
     animation: tl,
-    trigger: ".cover-area",
+    trigger: "#app",
     start: "top top",
     end: "+=2300",
-    duration:1000,
+    duration: 1000,
     scrub: 0.5,
     pin: true,
 });
